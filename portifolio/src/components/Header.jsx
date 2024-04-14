84fcb77f2b61e2d111c57842ac1c462437b785c0
Paulo2991 <paulovaz38034@gmail.com>
2024-04-14 15:03:23 -0300

    Portifolio

---
+++ portifolio/src/components/Header.jsx
@@ -0,0 +1,23 @@
+function Header(){
+  return (
+    <>
+       <header>
+           <nav>
+               <ul>
+                  <li>Home</li>
+                  <li>Sobre</li>
+                  <li>Projetos</li>
+               </ul>
+           </nav>
+           <section>
+              <div>
+                  <img />
+                  <p>Eu sou Paulo Henrique Vaz Barbosa Desenvolvedor Front-End</p>
+              </div>
+           </section>
+       </header>
+    </>
+  );
+}
+
+export default Header;
